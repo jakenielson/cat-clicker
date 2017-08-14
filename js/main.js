c33b410e374893747ac6f537fec7a6f66659910d
jakenielson <groakayt@gmail.com>
2017-08-14 14:54:28 -0700

    Update event listener loop

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ var count = [0, 0];
 var eventNum;
 
 for (var i = 0; i < cats.length; i++) {
-  cats[i].addEventListener('click', function(e){
-    eventNum = e.path[0].currentSrc.slice(58, 59) - 1;
-    count[eventNum] += 1;
-    counters[eventNum].innerText = count[eventNum];
-  }, false);
+  cats[i].addEventListener('click', (function(catNum){
+    return function() {
+      count[catNum] += 1;
+      counters[catNum].innerText = count[catNum];
+    };
+  })(i));
 }
