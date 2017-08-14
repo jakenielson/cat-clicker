1d92e20c4962ee12498880bf0dc945a0c114f2e5
jakenielson <groakayt@gmail.com>
2017-08-14 14:39:34 -0700

    Add second cat clicker

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-var catImg = document.getElementById('cat-img');
-var count = 0;
+var cats = document.getElementsByClassName('cat-img');
+var counters = document.getElementsByClassName('click-count');
+var count = [0, 0];
+var eventNum;
 
-catImg.addEventListener('click', function(){
-  count += 1;
-  document.getElementById('click-count').innerText = count;
-}, false);
+for (var i = 0; i < cats.length; i++) {
+  cats[i].addEventListener('click', function(e){
+    eventNum = e.path[0].currentSrc.slice(58, 59) - 1;
+    count[eventNum] += 1;
+    counters[eventNum].innerText = count[eventNum];
+  }, false);
+}
